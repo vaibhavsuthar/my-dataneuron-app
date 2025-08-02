a07df493e8d9b5cf245a81f7bf1a49740b6ca36b
Vaibhav Suthar <vaibhavsuthar9259@gmail.com>
2025-08-02 15:01:31 +0000

    feat: add all favicon icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         {/* Calendly widget styles */}
- <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
  <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
